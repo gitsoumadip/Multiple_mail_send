5d238b323863c5c1a607ea66f3ad21afd69430fe
gitsoumadip <soumadiphazra00@gmail.com>
2023-02-16 01:23:24 +0530

    mail send with upload excel file

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    // redirect('importExportView');
+    return redirect('importExportView');
 });
 
 Route::get('importExportView',[FileImportController::class,'importExportView']);
